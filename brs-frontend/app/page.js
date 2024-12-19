0b41ee8a2d99eda520fda93c8b8d0e01b9b7c8b6
maria <delapazferreras.maria@gmail.com>
2024-12-19 15:09:14 +0100

    Add footer

--- brs-frontend/app/page.js
+++ brs-frontend/app/page.js
@@ -1,11 +1,17 @@
-
+import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
-    <div>
+    <>
+    <header>
       <Navbar />
-    </div>
-    
+    </header>
+    <main>
+      <Hero />
+      <Footer/>
+    </main>
+    </>
   );
 }
