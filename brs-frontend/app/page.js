016362946ffdc4709deb852a858b705d7697e930
Abdullah <haranbaa97@gmail.com>
2024-12-24 01:03:01 +0100

    Add BookingForm, FeaturesSection, and PersistentBar components; update Home page layout

--- brs-frontend/app/page.js
+++ brs-frontend/app/page.js
@@ -2,18 +2,20 @@ import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import AboutUs from "@/components/AboutUs";
+import FeaturesSection from "@/components/FeaturesSection"; // Import the FeaturesSection component
 
 export default function Home() {
   return (
     <>
-    <header>
-      <Navbar />
-    </header>
-    <main>
-      <Hero />
-      <AboutUs />
-      <Footer/>
-    </main>
+      <header>
+        <Navbar />
+      </header>
+      <main>
+        <Hero />
+        <FeaturesSection /> {/* Add the FeaturesSection component here */}
+        <AboutUs />
+        <Footer />
+      </main>
     </>
   );
 }
