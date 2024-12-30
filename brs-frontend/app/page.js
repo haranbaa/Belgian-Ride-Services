ba2fd28c4b1632d186b09e077a6385c0aa981d2a
maria <delapazferreras.maria@gmail.com>
2024-12-30 10:18:27 +0100

    Fix typescript error, add about us to main page (still on work), fix contact page path

--- brs-frontend/app/page.js
+++ brs-frontend/app/page.js
@@ -19,6 +19,7 @@ export default function Home() {
         <BusinessSection />
         <CourierServiceSection /> {/* Add the CourierServiceSection component */}
         <EcoFriendlyFleet />
+        <AboutUs />
       </main>
       <Footer />
     </>
